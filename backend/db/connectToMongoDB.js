b6e3139704d0875d11a3b3d30cdb849faf420aba
Manya Samadhiya <manyasamadhiya2004@gmail.com>
2025-11-14 14:23:22 +0530

    minor change

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -2,7 +2,7 @@ const mongoose=require("mongoose");
 
 const connectToMongoDB=async()=>{
     try{
-        await mongoose.connect("mongodb://127.0.0.1:27017/chatapp");
+        await mongoose.connect("mongodb+srv://manya:m1a2n3y4a5@cluster0.byoqk0o.mongodb.net/?appName=Cluster0");
         console.log("connected to mongodb");
     }
     catch(error){
